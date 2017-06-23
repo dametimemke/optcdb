b46b16865b3f61c2d1076134d7cc5b9826c89184
Jan Raventós <janraven55@gmail.com>
2017-06-23 23:49:45 +0200

    Enabled #1652  Nekomamushi on Calculator

--- common/data/units.js
+++ common/data/units.js
@@ -1650,7 +1650,7 @@ window.units = [
     [ "", "Type", [ "Class1", "Class2" ], null, null, null, null, null, null, null, null, null, null, null, null, null ],
     [ "", "Type", [ "Class1", "Class2" ], null, null, null, null, null, null, null, null, null, null, null, null, null ],
     [ "Nekomamushi", "PSY", [ "Striker", "Powerhouse" ], 5, 30, null, 3, 70, 1066998, null, null, null, null, null, null, null ],
-    [ "'King of the Night' Nekomamushi", "PSY", [ "Striker", "Powerhouse" ], 6, 55, 4, 5, 99, 5000000, null, null, null, 3404, 1451, 352, null ],
+    [ "'King of the Night' Nekomamushi", "PSY", [ "Striker", "Powerhouse" ], 6, 55, 4, 5, 99, 5000000, 1, 1, 1, 3404, 1451, 352, null ],
     [ "Pedro", "INT", [ "Slasher", "Cerebral" ], 4, 15, null, 3, 50, 392109, null, null, null, null, null, null, null ],
     [ "Pedro of the Treetops", "INT", [ "Slasher", "Cerebral" ], 5, 30, 4, 4, 99, 4000000, 1, 1, 1, 2009, 1438, 325, null ],
     [ "Carrot", "QCK", [ "Fighter", "Free Spirit" ], 4, 15, null, 3, 50, 392109, null, null, null, null, null, null, null ],
